03bb74c69912cd048e4488fda5dee37e0b68c8bd
Ran Bao <worksev@gmail.com>
2012-07-31 21:07:19 +1200

    inti

---
+++ opengl_five_in_row/build/opengl_five_in_row.build/Debug/opengl_five_in_row.build/Objects-normal/x86_64/functions.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mac/Codes/opengl_five_in_row/opengl_five_in_row/functions.c \
+  /Users/mac/Codes/opengl_five_in_row/opengl_five_in_row/define.h
---
+++ opengl_five_in_row/build/opengl_five_in_row.build/Debug/opengl_five_in_row.build/Objects-normal/x86_64/versus.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mac/Codes/opengl_five_in_row/opengl_five_in_row/versus.c \
+  /Users/mac/Codes/opengl_five_in_row/opengl_five_in_row/define.h
