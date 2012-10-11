fe7e3b3ae608d064900cca04cc8e7affc69cf3cf
eamars <worksev@gmail.com>
2012-10-12 00:08:10 +1300

    init

---
+++ md5_Test/build/md5_Test.build/Release/md5_Test.build/Objects-normal/x86_64/md5.d
@@ -0,0 +1,2 @@
+dependencies: /Users/mac/Codes/md5_Test/md5_Test/md5.c \
+  /Users/mac/Codes/md5_Test/md5_Test/md5.h
--- md5_Test/md5_Test/md5funct.c
+++
@@ -1,9 +0,0 @@
-//
-//  md5hash.c
-//  md5_Test
-//
-//  Created by eamars on 9/10/12.
-//  Copyright (c) 2012 eamars. All rights reserved.
-//
-
-#include <stdio.h>
